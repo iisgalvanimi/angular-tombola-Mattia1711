ece6a639f473784deb6c3ab8509ed6e648cfc9a9
Mattia1711 <buonaguidi.mattia@iisgalvanimi.edu.it>
2020-04-20 15:39:59 +0000

    ../app.module.ts

--- Buonaguidi-tombola/src/app/app.module.ts
+++ Buonaguidi-tombola/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CartellaComponent } from './cartella/cartella.component';
@@ -13,6 +13,9 @@ import { CartellaComponent } from './cartella/cartella.component';
     BrowserModule,
   ],
   providers: [],
+  schemas: [
+    NO_ERRORS_SCHEMA
+      ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
