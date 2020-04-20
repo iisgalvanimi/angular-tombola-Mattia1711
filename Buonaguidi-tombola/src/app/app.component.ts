96f4100ad21898656b4b8f7faf8645674811bf6d
Mattia1711 <buonaguidi.mattia@iisgalvanimi.edu.it>
2020-04-20 14:08:04 +0000

    app.component.ts

--- Buonaguidi-tombola/src/app/app.component.ts
+++ Buonaguidi-tombola/src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Buonaguidi-tombola';
+  numeriUsciti: Array<number>=[];
+  ultimoNumero: Number;
+  generaNumero() {
+    var n = null;
+    while(n==null) {
+      let tmp = Math.floor(Math.random()*99+1);
+      let change = true;
+      for (var i in this.numeriUsciti) {
+        if (tmp == Number(i)) {
+          change = false;
+        }
+      }
+      if (change) {
+        n = tmp;
+      }
+    }
+    this.ultimoNumero=n;
+    this.numeriUsciti.push(n);
+    console.log(n);
+    document.getElementById("numero").innerHTML=n+"";
+    if (this.numeriUsciti.length == 100) {
+      console.log("Sono esauriti i numeri");
+      document.getElementById("bottone").setAttribute("disabled", "disabled");
+      alert("Numeri Eusariti")
+      return false;
+    }
+    return false;
+  }
 }
